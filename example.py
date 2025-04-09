96b8e16308ec25d01429826567c4d13b66c1f5c6
Ivan Novikov <ivannovikov1303@gmail.com>
2025-04-09 10:46:36 +0300

    np array compatibility

--- example.py
+++ example.py
@@ -5,15 +5,9 @@ import librosa
 audio_tensor = torch.randn(50000)
 
 model = create_model(pretrained=True)
-preds = model.predict(input_path='_-a-c3-3.wav')
-
-print(preds)
-
-audio_t = librosa.load('_-a-c3-3.wav', sr=16000)
-audio_t = audio_t[0]
 
 preds = model.predict(
-    input_tensor=audio_t,
+    input_tensor=audio_tensor,
     sample_rate=16000,
     device="cuda:0"
 )
@@ -21,7 +15,7 @@ preds = model.predict(
 print(preds)
 
 preds = model.predict(
-    input_tensors=[audio_t, audio_t],
+    input_tensors=[audio_tensor, audio_tensor],
     sample_rate=16000,
     device="cuda:0"
 )
